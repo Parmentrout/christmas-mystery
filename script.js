bd64c5f3925ff5f9b8fb00a1582aa3c0f108109f
Patrick Armentrout <patrick.armentrout@gmail.com>
2019-11-16 12:48:45 -0700

    Finishing...

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 $(() => {
     console.log( "ready!" );
-    //$('#decodedScreen').hide();
+    $('#decodedScreen').hide();
+    hideErrors();
 
     $('#drunk-form').submit((event) => {
         event.preventDefault();
+        hideErrors();
 
-        let password = $('input').first().val();
+        let password = $('input').first().val().toLowerCase();
 
+        let correctChars = password.includes('r') && password.includes('e') && password.includes('d')
+            && password.includes('h'); 
         // If you are reading this code, just know that you are cheating and I'm not mad, just disappointed...
-        if (password === 'redherring') {
+        if (password === 'redherring' || password === 'red herring') {
             $('#initialScreen').hide();
             $('#decodedScreen').show(1000);
+        } else if (correctChars) {
+            $('#badLetters').show();
+        }
+        else {
+            $('#badpassword').show();
         }
     });
+
+    function hideErrors() {
+        $('#badpassword').hide();
+        $('#badLetters').hide();
+    }
 });
