6dfb7600dacca659b38f28e8cf35b8fe081173c4
Patrick Armentrout <patrick.armentrout@gmail.com>
2019-11-18 19:49:32 -0700

    Work on website make better

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 $(() => {
-    console.log( "ready!" );
     $('#decodedScreen').hide();
     hideErrors();
 
@@ -10,21 +9,42 @@ $(() => {
         let password = $('input').first().val().toLowerCase();
 
         let correctChars = password.includes('r') && password.includes('e') && password.includes('d')
-            && password.includes('h'); 
+            && password.includes('h')  && password.includes('g'); 
+
         // If you are reading this code, just know that you are cheating and I'm not mad, just disappointed...
         if (password === 'redherring' || password === 'red herring') {
+            $('#errorMessage').text('Correct!');
             $('#initialScreen').hide();
             $('#decodedScreen').show(1000);
         } else if (correctChars) {
-            $('#badLetters').show();
+            setErrorMessage('Hmm, you have the letters right but out of order!')
+        } else if (submittedJournalEntry(password)) {
+            setErrorMessage('I see someone is just wildly guessing journal words, incorrect!')
         }
         else {
-            $('#badpassword').show();
+            setErrorMessage('Incorrect Password!');
         }
     });
 
     function hideErrors() {
-        $('#badpassword').hide();
-        $('#badLetters').hide();
+       setErrorMessage('');
+    }
+
+    function setErrorMessage(text) {
+        if (text) {
+            text += ' Need a hint? Remember you can send us a text for a clue!'
+        }
+        $('#errorMessage').text(text);
+    }
+
+    function submittedJournalEntry(text) {
+        let journalEntries = ['magical', 'brave', 'mood', 'confident', 'on point', 'super star', 'brilliant'];
+
+        for (let entry of journalEntries) {
+            if (text == entry) {
+                return true;
+            }
+        }
+        return false;
     }
 });
